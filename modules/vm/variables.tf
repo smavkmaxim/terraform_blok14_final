df055c7c7b514eb4d716abb2686ff629c09b1af4
smavkmaxim <121498385+smavkmaxim@users.noreply.github.com>
2025-10-08 13:29:49 +0800

    Update variables.tf

---
+++ modules/vm/variables.tf
@@ -0,0 +1,19 @@
+variable "custom_folder_id" {
+  type    = string
+  default = "b1ge32e4edrv1bn68jeo"
+}
+
+variable "custom_zone" {
+  type    = string
+  default = "ru-central1-d"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+
+variable "subnet_id" {
+  type    = string
+}
