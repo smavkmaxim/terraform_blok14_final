38b18b4b99b61e3c527561335175d381a5f7fa75
smavkmaxim <smavkmaxim@mail.ru>
2025-10-08 03:02:30 +0000

    sdfsa

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,12 @@
+variable "custom_folder_id" {
+  type    = string
+}
+
+variable "custom_zone" {
+  type    = string
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "subnet-1" {
-  value = yandex_vpc_subnet.subnet-1.id
-}
-
-output "network-1" {
-  value = yandex_vpc_network.network-1.id
-}
